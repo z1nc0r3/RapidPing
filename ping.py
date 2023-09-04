e643d6b7c180d0bd7a681c3392104e4d23861a3e
Lasith Manujitha <Manujitha98@gmail.com>
2023-09-04 15:16:55 +0530

    Feature to keep track of the counter

--- ping.py
+++ ping.py
@@ -1,20 +1,35 @@
 import requests
 
-output = open('output.txt', 'a')
-counter = 1
+def load_or_create_counter():
+    try:
+        with open('counter.txt', 'r') as counter_file:
+            return int(counter_file.read())
+    except FileNotFoundError:
+        return int(input('Enter the starting point: '))
 
-with open('domain.txt', 'r') as f :
-    for line in f :
+counter = load_or_create_counter()
+
+output = open('output.txt', 'a')
+
+with open('domain.txt', 'r') as f:
+    lines = f.readlines()
+    start = counter
+
+    for line in lines[start - 1:]:
         print(counter, end='\r')
-        link = line.split('\n')[0]
-        
+        link = line.strip()
+
         try:
-            r = requests.get(link, timeout=0.8)
+            r = requests.get(link, timeout=0.65)
             if r.status_code == 200:
                 print(link, ' @Success!')
                 output.write(line)
                 output.flush()
         except Exception as e:
             pass
-            
+
         counter += 1
+
+        with open('counter.txt', 'w') as counter_file:
+            counter_file.write(str(counter))
+            counter_file.flush()
